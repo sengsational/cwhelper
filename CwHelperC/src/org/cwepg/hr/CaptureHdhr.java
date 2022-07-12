aaf479cd5c7ebba4d0b051f5867c759bd08af90b
sengsational <buhtig.frog4food@recursor.net>
2022-07-12 17:43:48 -0400

    throw correct error on command line lock required failure

--- CwHelperC/src/org/cwepg/hr/CaptureHdhr.java
+++ CwHelperC/src/org/cwepg/hr/CaptureHdhr.java
@@ -95,7 +95,7 @@ public class CaptureHdhr extends Capture implements Runnable {
                 forceRequired = report(cl) != null && report(cl).indexOf("resource locked") > -1;
                 //DRS 20220710 - Comment 1, Add 1 - Remove force recording...let this recording fail and later schedule a replacement, if possible.
                 //if (forceRequired) clearLockByForce(tuner.id, tuner.number);
-                if (forceRequired) throw new Exception("WARNING: The HDHR device was in use.  This capture will not proceed.");
+                if (forceRequired) throw new DeviceUnavailableException("WARNING: The HDHR device was in use.  This capture will not proceed.");
             } while (commandFailure || (forceRequired && forceCount++ < 1));
     
             if (this.target.isWatch()){
