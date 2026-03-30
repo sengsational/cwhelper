a45d9f001207ae4075ac39b7f5f1c42072b514b4
owner <buhtig.frog4food@recursor.net>
2026-03-30 14:55:06 -0400

    update build for both Windows and Linux

--- CwHelperC/src/com/cwepg/build/JreFinder.java
+++ CwHelperC/src/com/cwepg/build/JreFinder.java
@@ -1,32 +1,44 @@
 package com.cwepg.build;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class JreFinder {
-    public static String getDynamicJrePath() {
-        File pluginsDir = new File("/home/owner/Eclipse/plugins");
-        
+    public static String getDynamicJrePath() throws Exception {
+    	boolean isWindows = System.getProperty("os.name").toLowerCase().contains("win");
+    	File pluginsDir = null;
+    	if (isWindows) {
+    		pluginsDir = new File("C:\\Users\\tmpet\\.p2\\pool\\plugins");
+    	} else {
+            pluginsDir = new File("/home/owner/Eclipse/plugins");
+    	}
+
         if (pluginsDir.exists() && pluginsDir.isDirectory()) {
             // Filter for folders starting with the JustJ JRE prefix
-            File[] matches = pluginsDir.listFiles((dir, name) -> 
-                name.startsWith("org.eclipse.justj.openjdk.hotspot.jre.full.linux"));
+            File[] matches = pluginsDir.listFiles(new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					return name.startsWith("org.eclipse.justj.openjdk.hotspot.jre.full");
+				}
+			});
 
             if (matches != null && matches.length > 0) {
                 // Sort to get the latest version if multiple exist
                 Arrays.sort(matches);
                 File latestJreDir = matches[matches.length - 1];
-                
+
                 // Construct the path to the internal /jre folder
-                return new File(latestJreDir, "jre").getAbsolutePath() + "/";
+                return new File(latestJreDir, "jre").getAbsolutePath() + File.separator;
             }
         }
-        return null; // Fallback or handle error
+        throw new Exception("ERROR: Looked in [" + pluginsDir + "] for [org.eclipse.justj.openjdk.hotspot.jre.full ] but did not find it.");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String JRE_PATH = getDynamicJrePath();
         System.out.println("Detected JRE: " + JRE_PATH);
+        //Detected JRE [/home/owner/Eclipse/plugins/org.eclipse.justj.openjdk.hotspot.jre.full.linux.x86_64_21.0.10.v20260205-0638/jre/]
     }
 }
--- CwHelperC/src/module-info.java
+++ CwHelperC/src/module-info.java
@@ -17,7 +17,38 @@ module cwhelper {
     requires ucanaccess;
 }
 
-/*
+/** This worked to generate output on Linux with 1097 version fat jar
+ * 
+jlink \
+  --module-path $JAVA_HOME/jmods \
+  --add-modules java.base \
+  --add-modules java.compiler \
+  --add-modules java.desktop \
+  --add-modules java.naming \
+  --add-modules java.scripting \
+  --add-modules java.security.jgss \
+  --add-modules java.security.sasl \
+  --add-modules java.sql \
+  --add-modules jdk.httpserver \
+  --add-modules jdk.unsupported \
+  --add-modules java.datatransfer \
+  --add-modules java.logging \
+  --add-modules java.transaction.xa \
+  --add-modules java.xml \
+  --strip-debug \
+  --no-man-pages \
+  --no-header-files \
+  --compress=2 \
+  --output ./custom-runtime
+ * 
+ * jlink did not like it when these were included:
+ * 
+ *  --add-modules com.sun.jna.platform \
+ *  --add-modules com.sun.jna \
+ */
+
+
+/* Earlier suggestion for jlink that did not work
 jlink \
   --module-path $JAVA_HOME/jmods \
   --add-modules java.security.sasl \
